59ad9be6989de8eee948510b850851105ecad22e
Ken Reed <kenakioreed@gmail.com>
2019-09-02 19:11:34 -0400

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 
 //! Simple crate that will reorder a slice based on a slice of indicies without an auxilliary array. See https://www.geeksforgeeks.org/reorder-a-array-according-to-given-indexes/
 
-extern crate unchecked_index;
+//extern crate unchecked_index;
 
 
 #[test]
