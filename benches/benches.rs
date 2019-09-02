b9f1be795f9cd322cfe10739c184bc10139ac46e
Ken Reed <kenakioreed@gmail.com>
2019-09-02 15:36:47 -0400

    updates

--- benches/benches.rs
+++ benches/benches.rs
@@ -4,13 +4,19 @@ extern crate rand;
 extern crate test;
 use test::*;
 
-fn random_perm(num:usize)->(Vec<usize>,Vec<u32>){
+
+#[derive(Copy,Clone)]
+struct Bag{
+    _a:[usize;16],
+}
+
+fn random_perm(num:usize)->(Vec<Bag>,Vec<u32>){
     use rand::{Rng, SeedableRng, StdRng};
 
     let seed: &[_] = &[1, 2, 3, 4];
     let mut rng: StdRng = SeedableRng::seed_from_u64(2432);
 
-    let mut vec:Vec<_>=(0usize..num).collect();
+    let mut vec:Vec<_>=(0usize..num).map(|i|Bag{_a:[i;16]}).collect();
     rng.shuffle(&mut vec);
 
     let mut vec2:Vec<_>=(0u32..num as u32).collect();
@@ -23,7 +29,7 @@ fn random_perm(num:usize)->(Vec<usize>,Vec<u32>){
 #[bench]
 fn bench_reorder_index(b:&mut Bencher){
     b.iter(||{
-        let (mut a,mut b)=random_perm(5000);
+        let (mut a,mut b)=random_perm(500_000);
         reorder::reorder_index(&mut a,&mut b);
         black_box((a,b));
     });
@@ -32,7 +38,7 @@ fn bench_reorder_index(b:&mut Bencher){
 #[bench]
 fn bench_reorder_index_aux(b:&mut Bencher){
     b.iter(||{
-        let (mut a,mut b)=random_perm(5000);
+        let (mut a,mut b)=random_perm(500_000);
         reorder::reorder_index_aux(&mut a,&mut b);
         black_box((a,b));
     });
--- src/lib.rs
+++ src/lib.rs
@@ -10,18 +10,29 @@ fn test(){
     let mut v2=[3,0,4,1,2];
     let mut a1=v1.clone();
     let mut a2=v2.clone();
+    let mut b1=v1.clone();
+    let mut b2=v2.clone();
+    
     
     reorder_index(&mut v1,&mut v2);
     reorder_index_aux(&mut a1,&mut a2);
 
-    for (a,b) in v1.iter().zip(a1.iter()){
+    let mut bla=Vec::new();
+    bla.extend_from_slice(&v1);
+    let a3:Vec<usize>=reorder_index_aux_vec(bla,&mut a2);
+
+
+    for ((a,b),c) in v1.iter().zip(a1.iter()).zip(a3.iter()){
         assert_eq!(a,b);
+        assert_eq!(b,c);
     }
 
 
     for (a,b) in v2.iter().zip(a2.iter()){
         assert_eq!(a,b);
     }
+
+
 
 }
 
@@ -48,36 +59,77 @@ pub fn swap_index(bla:impl ExactSizeIterator<Item=u32>)->Vec<u32>{
 
 
 #[inline]
-pub fn reorder_index_aux<'a,A:Copy>(arr:&'a mut [A],index:&mut [u32]){
-    let mut v=Vec::new();
-    v.extend_from_slice(arr);
+pub fn reorder_index_aux<'a,A>(arr:&mut [A],index:&mut [u32]){
+    
+    
+    let mut v=Vec::with_capacity(arr.len());
+    unsafe{
+        std::ptr::copy_nonoverlapping(arr.as_ptr(), v.as_mut_ptr(), arr.len());
+        v.set_len(arr.len());
+    }
 
-    for (i,(a,index)) in v.iter().zip(index.iter_mut()).enumerate(){
-        arr[*index as usize]=*a;
+    for (i,(a,index)) in v.drain(..).zip(index.iter_mut()).enumerate(){
+        let res=core::mem::replace(&mut arr[*index as usize],a);
+
+        //arr[*index as usize]=a;
+        *index=i as u32;
+        core::mem::forget(res);
+    }
+    
+}
+
+
+
+
+#[inline]
+pub fn reorder_index_aux_vec<'a,A>(mut arr:Vec<A>,index:&mut [u32])->Vec<A>{
+    
+    
+    let mut v=Vec::with_capacity(arr.len());
+    unsafe{v.set_len(arr.len())};
+    let vv=unsafe{std::slice::from_raw_parts_mut(v.as_mut_ptr(),v.len())};
+
+
+    for (i,(a,index)) in arr.drain(..).zip(index.iter_mut()).enumerate(){
+        vv[*index as usize]=a;
         *index=i as u32
     }
+
+    v
+    
 }
 
 
 #[inline]
-pub fn reorder_index<'a,A:Copy>(arr:&'a mut [A],index:&mut [u32]){
+pub fn reorder_index<'a,A>(arr:&'a mut [A],index:&mut [u32]){
 
     
     assert_eq!(arr.len(),index.len());
 
-    //let mut index=unsafe{unchecked_index::unchecked_index(index)};
 
+    
 
     for i in 0..arr.len(){
         while index[i] as usize!=i{
             let old_target_i = index[index[i] as usize];
-            let old_target_e = arr[index[i] as usize];
+            
 
-            arr[index[i] as usize] = arr[i];
+            let mut old_target_e:A=unsafe{std::mem::MaybeUninit::<A>::zeroed().assume_init()};
+            core::mem::swap(&mut old_target_e,&mut arr[index[i] as usize]);
+            //let old_target_e = arr[index[i] as usize];
+
+            //arr[index[i] as usize] = arr[i];
+            let kk:&mut _=unsafe{&mut *(&mut arr[i] as *mut _)};
+            core::mem::swap(&mut arr[index[i] as usize],kk);
+
             index[index[i] as usize]=index[i];
 
             index[i]=old_target_i;
-            arr[i] = old_target_e;
+
+            //arr[i] = old_target_e;
+            core::mem::swap(&mut arr[i],&mut old_target_e);
+
+            core::mem::forget(old_target_e);
         }
     }
 }
