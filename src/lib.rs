046e3d67f6d5c8eb41b0df2e79c4cdb3fae85d92
tiby312 <kenakioreed@gmail.com>
2021-04-05 09:21:25 -0400

    bugfix

--- src/lib.rs
+++ src/lib.rs
@@ -31,28 +31,28 @@ fn test(){
     for (a,b) in v2.iter().zip(a2.iter()){
         assert_eq!(a,b);
     }
-
-
-
 }
 
 
-
-
-
-
+///
+/// Unsafe since the size of the size hint of the iterator is not
+/// guarenteed to be equal to the number of items produced by
+/// the iterator.
+///
+/// The user must also guarentee that the provided iterator is a permutation
+/// of the indicies.
+///
 #[inline]
-pub fn swap_index(bla:impl ExactSizeIterator<Item=u32>)->Vec<u32>{
+pub unsafe fn swap_index(bla:impl ExactSizeIterator<Item=u32>)->Vec<u32>{
     let len=bla.len();
     let mut vec=Vec::with_capacity(len);
-    let arr:&mut [u32]=unsafe{std::slice::from_raw_parts_mut(vec.as_mut_ptr(),bla.len())};
+    let arr:&mut [u32]=std::slice::from_raw_parts_mut(vec.as_mut_ptr(),bla.len());
     for (i,a) in bla.enumerate(){
         arr[a as usize]=i as u32;
     }
 
-    unsafe{
     vec.set_len(len);
-    }
+    
     vec
 }
 
